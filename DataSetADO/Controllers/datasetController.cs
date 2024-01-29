4811374d8ce10597812bad47c3068f93a88b7375
rock-32 <shaanarshaqu123@gmail.com>
2024-01-29 21:23:12 +0530

    Add project files.

---
+++ DataSetADO/Controllers/datasetController.cs
@@ -0,0 +1,25 @@
+﻿using DataSetADO.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataSetADO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class datasetController : ControllerBase
+    {
+        private readonly IStudents _students;
+        public datasetController(IStudents students)
+        {
+            _students=students;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            return Ok(_students.GetData());
+        }
+    }
+}
---
+++ DataSetADO/Model/Students.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using System.Data;
+
+namespace DataSetADO.Model
+{
+
+    public interface IStudents
+    {
+        List<StudentDTO> GetData();
+    }
+    public class Students: IStudents
+    {
+       
+        private readonly IConfiguration _configuration;
+        private readonly string connectionstring;
+        public Students(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connectionstring = configuration["ConnectionStrings:DefaultConnection"];
+        }
+
+
+        public List<StudentDTO> GetData()
+        {
+           
+            using (SqlConnection connect =  new SqlConnection(connectionstring)) 
+            {
+                connect.Open();
+                using (SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM students_list", connect))
+                {
+                    SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+                    DataSet dataSet = new DataSet();
+                    dataAdapter.Fill(dataSet, "students");
+                    List<StudentDTO> student_data = new List<StudentDTO>();
+                    foreach (DataRow row in dataSet.Tables["students"].Rows)
+                    {
+                        StudentDTO student = new StudentDTO
+                        {
+                            Id = Convert.ToInt32(row["Id"]),
+                            Name = row["Name"].ToString(),
+                            Age = Convert.ToInt32(row["Age"])
+                        };
+
+                        student_data.Add(student);
+                    }
+                    return student_data;
+                }
+            }
+            
+        }
+
+
+
+        public void deleteData(int id)
+        {
+            using(SqlConnection connect = new SqlConnection(connectionstring))
+            {
+                connect.Open();
+                using (SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM students_list", connect))
+                {
+                    SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+                    DataSet dataSet = new DataSet();
+                    dataAdapter.Fill(dataSet, "students");
+                    DataRow rowToDelete = dataSet.Tables["Students"].Rows.Find(id);
+                    if(rowToDelete != null)
+                    {
+                        rowToDelete.Delete();
+                        dataAdapter.Update(dataSet, "Students");
+                    }
+                }
+            }
+        }
+
+
+
+
+    }
+}
